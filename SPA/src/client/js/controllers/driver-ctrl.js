1cbc82322b4883d60992ef6d2e87094d42dd3309
Phong Nguyen <phongnguyen@ccintegraton.com>
2017-12-26 14:27:55 +0700

    Google map icons

--- SPA/src/client/js/controllers/driver-ctrl.js
+++ SPA/src/client/js/controllers/driver-ctrl.js
@@ -13,22 +13,37 @@ function driverCtrl($scope, $rootScope, helper, $location, $http) {
         var mapCanvas = document.getElementById("driverMap");
         var mapOptions = { center: myCenter, zoom: 16 };
         var map = new google.maps.Map(mapCanvas, mapOptions);
-        var marker = new google.maps.Marker(
+        var markerCustomer = new google.maps.Marker(
             {
                 map: map,
+                // icon: "https://maps.google.com/mapfiles/kml/shapes/man.png",
+                icon: "/img/client.png",
+                //label:"AAA",
                 draggable: true,
                 animation: google.maps.Animation.DROP,
                 position: { lat: 10.791424, lng: 106.6972366 }
             });
-        marker.setMap(map);
+        markerCustomer.setMap(map);
+
+        var markerDriver = new google.maps.Marker(
+            {
+                map: map,
+                // icon: "https://maps.google.com/mapfiles/kml/shapes/bus.png",
+                icon: "/img/car.png",
+                //label:"AAA",
+                draggable: true,
+                animation: google.maps.Animation.DROP,
+                position: { lat: 10.789410, lng: 106.6952366 }
+            });
+        markerDriver.setMap(map);
 
         var infowindow = new google.maps.InfoWindow({
-            content: 'Di chuyển để định vị'
+            content: 'Điểm đón'
         });
-        infowindow.open(map, marker);
+        infowindow.open(map, markerCustomer);
 
-        google.maps.event.addListener(marker, 'dragend', function () {
-            geocodePosition(marker.getPosition());
+        google.maps.event.addListener(markerCustomer, 'dragend', function () {
+            geocodePosition(markerCustomer.getPosition());
         });
 
         function geocodePosition(pos) {
@@ -38,6 +53,7 @@ function driverCtrl($scope, $rootScope, helper, $location, $http) {
                     latLng: pos
                 },
                 function (results, status) {
+                    console.log(results)
                     if (status == google.maps.GeocoderStatus.OK) {
                         alert("Bạn vừa thay đổi thành: " + results[0].formatted_address);
                     }
