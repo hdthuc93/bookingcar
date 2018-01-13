3a7494757b3bdc7944da1e1a61396a2ca819afb8
hdthuc93 <hdthuc93@gmail.com>
2018-01-13 16:14:13 +0700

    fix CORS

--- server_side/src/configs/server_config.js
+++ server_side/src/configs/server_config.js
@@ -15,7 +15,12 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '../../../dist/index.html');
 });
 
-app.options('*', cors());
+// app.options('*', cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use('/api', routes);
 
 export default app;
